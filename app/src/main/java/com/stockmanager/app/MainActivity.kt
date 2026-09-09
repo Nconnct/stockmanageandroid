245f98f16396e70fae720ffab8140623b2131783
Rajendra shil <104653548+Nconnct@users.noreply.github.com>
2026-09-09 11:16:48 +0530

    Fix formatting issue in MainActivity.kt

--- app/src/main/java/com/stockmanager/app/MainActivity.kt
+++ app/src/main/java/com/stockmanager/app/MainActivity.kt
@@ -54,4 +54,4 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
     val navController = findNavController(R.id.nav_host_fragment)
     return navController.navigateUp() || super.onSupportNavigateUp()
-    }
+    } }
